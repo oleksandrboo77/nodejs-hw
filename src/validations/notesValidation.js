3dd5e96bf9b09aa10aab00282b7d901e68113e5c
oleksandrboo77 <oleksandrboo77@gmail.com>
2025-10-15 23:00:30 +0300

    fix import

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,13 +1,13 @@
 import { Joi, Segments } from 'celebrate';
-import { TAGS } from '../constants/tags';
+import { TAGS } from '../constants/tags.js';
 import { isValidObjectId } from 'mongoose';
 
 export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
-    page: Joi.number().integer.min(1).default(1),
-    perPage: Joi.number().integer.min(5).max(20).default(10),
-    tag: Joi.string.valid(...TAGS),
-    search: Joi.string().trim.allow(''),
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+    tag: Joi.string().valid(...TAGS),
+    search: Joi.string().trim().allow(''),
   }),
 };
 
